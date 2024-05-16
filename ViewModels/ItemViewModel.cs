d73748ce707b369618803b40de9f217a9a61bf91
awesumscott <scott@scottlowe.us>
2024-05-16 02:18:20 -0400

    .NET 8, loop rename

---
+++ ViewModels/ItemViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Blockage.LevelEditor;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Blockage.Editor.ViewModels;
+
+internal sealed partial class ItemViewModel : ObservableObject {
+	public event PropertyChangedEventHandler PropertyChanged;
+
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(Name))]
+	private int _x;
+
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(Name))]
+	private int _y;
+
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(Name))]
+	private int _width;
+
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(Name))]
+	private int _height;
+
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(Name))]
+	private ObjectType _type;
+
+	public string Name => $"{Type} ({X}, {Y}) {Width}x{Height}";
+
+	public ItemViewModel Clone() => new() {
+		X = X,
+		Y = Y,
+		Width = Width,
+		Height = Height,
+		Type = Type
+	};
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,60 @@
+﻿using Blockage.LevelEditor;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace Blockage.Editor.ViewModels;
+
+internal sealed partial class MainWindowViewModel : ObservableObject {
+	private static readonly int _CELLS = 16;
+	private int _cellWidth;
+	private int _cellHeight;
+	private SlideGrid _game;
+
+	[ObservableProperty]
+	private ItemViewModel _selectedItem;
+
+	[ObservableProperty]
+	private Rectangle _levelRect;
+
+
+	[ObservableProperty]
+	private int _levelWidth = 10;
+	partial void OnLevelWidthChanged(int value) => UpdateLevelRect();
+
+	[ObservableProperty]
+	private int _levelHeight = 10;
+	partial void OnLevelHeightChanged(int value) => UpdateLevelRect();
+
+
+
+	private BindingList<ItemViewModel> Items = new();
+
+
+
+	private void UpdateLevelRect() {
+		//if (LevelWidth != null && int.TryParse(LevelWidth.Text, out var w)) {
+		//	_levelWidth = w;
+		//	LevelRect.Width = w * _cellWidth + 5;
+		//}
+		//if (LevelHeight != null && int.TryParse(LevelHeight.Text, out var h)) {
+		//	_levelHeight = h;
+		//	LevelRect.Height = h * _cellHeight + 5;
+		//}
+		LevelRect.Width = LevelWidth * _cellWidth + 5;
+		LevelRect.Height = LevelHeight * _cellHeight + 5;
+	}
+
+	[RelayCommand]
+	private void _startGame() {
+		UpdateLevelRect();
+		_game = new SlideGrid();
+		_game.Init(LevelWidth, LevelHeight, Items.Select(x => x.Clone()).ToList());
+	}
+}
